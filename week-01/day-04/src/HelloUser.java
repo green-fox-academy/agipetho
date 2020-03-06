1c49aa4b1ad44972ee6eac5dead66269ce8b95b4
Agnes Petho <apetho12@gmail.com>
2020-03-06 10:45:46 +0100

    scanner exercises

---
+++ week-01/day-04/src/HelloUser.java
@@ -0,0 +1,16 @@
+
+
+import java.util.Scanner;
+
+public class ScannerTeszt {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ird be a neved");
+        String yourName = scanner.nextLine();
+        System.out.println("Hello, " + yourName + "!");
+
+    }
+}
+
+
