0d1cf0bb823fefbdf459c8429077d9518535965d
Agnes Petho <apetho12@gmail.com>
2020-03-08 23:32:24 +0100

    diamond exercise improved

--- week-01/day-05/src/DrawDiamond.java
+++ week-01/day-05/src/DrawDiamond.java
@@ -2,23 +2,23 @@ import java.util.Scanner;
 public class DrawDiamond {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Írj be egy számot!");
-        int diamondNumber = scanner.nextInt();
-        double dividedNumber = (double) diamondNumber / 2;
-        long roundhalfDiamond = Math.round (dividedNumber);
-        int halfDiamond = diamondNumber / 2;
+        System.out.println("Type the number of row of the diamond!");
+        int diamondCnt = scanner.nextInt();
+        double dividedNumber = (double) diamondCnt / 2;
+        int roundhalfDiamond = (int) Math.round (dividedNumber);
+        int halfDiamond = diamondCnt / 2;
 
-        for (int i = 1; i <= roundhalfDiamond; i++) {
-            for (int j = 0; j < (halfDiamond - i); j++) {
+        for (int i = 1; i <= halfDiamond; i++) {
+            for (int j = 0; j < (roundhalfDiamond - i); j++) {
                 System.out.print(" ");
             }
             for (int k = 0; k < ((2*i) -1); k++) {
                 System.out.print("*");
             }
-//            System.out.println();
+            System.out.println();
         }
-        for (int i = halfDiamond; i > 0; i--) {
-            for (int j = 0; j < (halfDiamond - i); j++) {
+        for (int i = roundhalfDiamond; i > 0; i--) {
+            for (int j = 0; j < (roundhalfDiamond - i); j++) {
                 System.out.print(" ");
             }
             for (int k = 0; k < ((2*i) -1); k++) {
