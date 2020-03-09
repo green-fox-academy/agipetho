90689cb938d611e1ee713936cd993d51a1ea49ba
Agnes Petho <apetho12@gmail.com>
2020-03-09 15:59:31 +0100

    Arrays exercises finished

---
+++ week-02/day-01/src/SumAll.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+// - Create an array variable named `numbers`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Print the sum of the elements in `numbers`
+public class SumAll {
+    public static void main(String[] args) {
+        int [] numbers = {3, 4, 5, 6, 7};
+        int a = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            a += numbers [i];
+        }
+        System.out.println(a);
+    }
+}
---
+++ week-02/day-01/src/SwapElements.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+// - Create an array variable named `orders`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `orders`
+public class SwapElements {
+    public static void main(String[] args) {
+        String [] orders = {"first", "second", "third"};
+        for (int i = 0; i < orders.length; i++) {
+            String temp = orders [0];
+            orders [0] = orders [2];
+            orders [2] = temp;
+        }
+        System.out.println(Arrays.toString(orders));
+    }
+}
