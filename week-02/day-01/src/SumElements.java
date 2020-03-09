e38724277201f255920488c1a9c550df12732f04
Agnes Petho <apetho12@gmail.com>
2020-03-09 14:00:58 +0100

    first 5 array exercises pushed

---
+++ week-02/day-01/src/SumElements.java
@@ -0,0 +1,12 @@
+// - Create an array variable named `numbers`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+public class SumElements {
+    public static void main(String[] args) {
+        int [] numbers = {54, 23, 66, 12};
+//        int a = numbers [1];
+//        int b = numbers [2];
+//        System.out.println(a + b);
+        System.out.println(numbers [1] + numbers [2]);
+    }
+}
---
+++ week-02/day-01/src/Test.java
@@ -0,0 +1,16 @@
+//public class Test {
+//    public static void main(String[] args) {
+//        int h = 12;
+//        for (int i = 0; i < ; i++) {
+//            for (int j = 0; j < ; j++) {
+//
+//                for (:
+//                     ) {
+//
+//                }
+//            }
+//        }
+//    }
+//
+//
+//}
