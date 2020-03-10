953a2d22267dfaa2badd10ca8164704a98787b62
Agnes Petho <apetho12@gmail.com>
2020-03-10 23:55:10 +0100

    exercises with lists and maps added

---
+++ week-02/day-02/src/PersonalFinance.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class PersonalFinance {
+    public static void main(String[] args) {
+        ArrayList <Integer> mySpendings = new ArrayList();
+        mySpendings.add(500);
+        mySpendings.add(1000);
+        mySpendings.add(1250);
+        mySpendings.add(175);
+        mySpendings.add(800);
+        mySpendings.add(120);
+        System.out.println("How much did we spend? :" + spendingCalc(mySpendings));
+    }
+    public static int spendingCalc (ArrayList<Integer> input){
+    int sum = 0;
+        for(Integer i : input){
+            sum += i;
+        }
+        return sum;
+    }
+}
