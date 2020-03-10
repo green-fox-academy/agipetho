a3a03d6a5133b33b12a2c9bf40cb7cb1a3b39ad3
Agnes Petho <apetho12@gmail.com>
2020-03-10 08:55:55 +0100

    UNique and SubInt started not yet finished

---
+++ week-02/day-01/src/Unique.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+//public class Unique {
+//    public static void main(String[] args) {
+        //  Create a function that takes a list of numbers as a parameter
+        //  Returns a list of numbers where every number in the list occurs only once
+
+        //  Example
+//        System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+        //  should print: `[1, 11, 34, 52, 61]`
+//    }
+//}
+
+//my idea: copy the original array and then check the copied elements one by one
+//public class Unique {
+//            public static void main(String[] args) {
+//            int [] listOfNumbers = {1, 11, 34, 11, 52, 61, 1, 34};
+//            System.out.println(unique(listOfNumbers));
+//            }
+//            public static int [] unique (int [] a ){
+//
+//                return
+//            }
+//
+//}
