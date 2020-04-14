3a8e699e62680cf43e7d83cf9adfc293fbe7ba91
Agnes Petho <apetho12@gmail.com>
2020-04-14 15:30:15 +0200

    exercise9 added

--- week-06/day-01/src/exercise9/FindFrequencyInAString/Exercise9.java
+++ week-06/day-01/src/exercise9/FindFrequencyInAString/Exercise9.java
@@ -5,21 +5,24 @@ Write a Stream Expression to find the frequency of characters in a given string!
 package exercise9.FindFrequencyInAString;
 
 
+import java.security.Key;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Exercise9 {
   public static void main(String[] args) {
     String string = "Write a Stream Expression to find the frequency of characters in a given string!";
-    //Stream<Character> characterStream = string.chars();
-  //  IntStream intStream = string.chars();
 
-   //IntStream intStream = testString.chars();
-    //.mapToObj(c -> (char) c);
-
-
+    Map<String, Long> frequentChars = Arrays.stream(string.split(""))
+        .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+    frequentChars.forEach((k, v) -> System.out.println(k + ":" + v));
+    //     .forEach((k, v) -> System.out.println(k + ":" + v);
   }
 }
+
