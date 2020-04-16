8166b0b639fa5b1489defd78effdd115094b3e61
Agnes Petho <apetho12@gmail.com>
2020-04-16 20:50:31 +0200

    HelloWorldRestApp started

---
+++ week-06/day-03/src/main/java/controllers/Greeting.java
@@ -0,0 +1,21 @@
+/*
+Add controllers package to your Spring project.
+Create Greeting class with long id and String content fields.
+Create getters for the fields
+Create a constructor using all fields
+Add HelloRESTController class to controllers package and add @RestController annotation to this class.
+Add greeting method and use @RequestMapping annotation.
+Create and return a Greeting object when it is called.
+Recompile app (use the "Make Project (Ctrl-F9" at the top of your project if you've edited the existing project and are restarting)
+Open http://localhost:8080/greeting in your web browser
+Your output should look like:
+{
+  "id": 1,
+  "content": "Hello, World!"
+}
+Hell
+ */
+package controllers;
+
+public class Greeting {
+}
