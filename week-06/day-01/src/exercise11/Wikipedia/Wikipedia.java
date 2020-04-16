cb4b40350a99125ff0acc3ea3c0c2be6090836f0
Agnes Petho <apetho12@gmail.com>
2020-04-16 11:59:42 +0200

    .gitignore updated

--- week-06/day-01/src/exercise11/Wikipedia/Wikipedia.java
+++ week-06/day-01/src/exercise11/Wikipedia/Wikipedia.java
@@ -24,12 +24,12 @@ public class Wikipedia {
       e.printStackTrace();
     }
     String wikipediaString = lines.toString();
-    String wikipediaStringWithoutPunctuation = wikipediaString.replaceAll("\\p{Punct}", " ");
+    String wikipediaStringWithoutPunctuation = wikipediaString.replaceAll("\\p{Punct}", " ").toLowerCase();
     //alternative found by googling:
     // String wikipediaStringWithoutPunctuation = wikipediaString.replaceAll("\\W", "");
+    //TODO to lower case conversion!
     Map<String, Long> mostFrequentWords = Arrays.stream(wikipediaStringWithoutPunctuation.split(" "))
         .collect(Collectors.groupingBy(w -> w, Collectors.counting()));
-
     mostFrequentWords.remove("");
 
     List<Map.Entry<String, Long>> result = mostFrequentWords.entrySet().stream()
--- week-06/day-01/src/exercise12/StarWars/StarWars.java
+++ week-06/day-01/src/exercise12/StarWars/StarWars.java
@@ -16,8 +16,60 @@ The result should be a Map<String, Map<String, Integer>>
 
 package exercise12.StarWars;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StarWars {
   public static void main(String[] args) {
+    String fileName = "files/sw.txt";
+    Path filePath = Paths.get(fileName);
+    List<String> lines = new ArrayList<>();
+    try {
+      lines = Files.readAllLines(filePath);
+    } catch (IOException e) {
+      System.out.println("Something went wrong with reading");
+      System.exit(-1);
+    }
+    lines.stream()
+        .
+    // String[] elementsOfTheLine = line.split(" ");
 
   }
 }
+/*
+ public static Map<String, Long> getFirstTenCommonWord(String fileName) {
+    List<String> lines = new ArrayList<>();
+    try {
+      lines = Files.readAllLines(Paths.get(fileName));
+    } catch (NoSuchFileException e) {
+      System.out.println("File doesn't exists");
+      System.exit(-1);
+    } catch (IOException e) {
+      System.out.println("Something went wrong with reading");
+      System.exit(-1);
+    }
+    Map<String, Long> firstHoundredCommonWordWithFrequencies =
+        lines.stream()
+            .flatMap(line -> Arrays.stream(line.split(" ")))
+            .map(word -> word.replaceAll("[^a-zA-Z]", "").toLowerCase().trim())
+            .filter(word -> word.length() > 0)
+            .collect(Collectors.groupingBy(word -> word, Collectors.counting()))
+            .entrySet()
+            .stream()
+            .sorted((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()))
+            .limit(100)
+            .collect(Collectors.toMap(
+                word -> word.getKey(),
+                word -> word.getValue(),
+                (k, v) -> {
+                  throw new IllegalStateException(String.format("Duplicate key %s", k));
+                },
+                LinkedHashMap::new
+            ));
+    return firstHoundredCommonWordWithFrequencies;
+  }
+ */
