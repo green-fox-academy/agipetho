9a6521e6a14d50d54fca61ad45d0197e8d6ae9e3
Agnes Petho <apetho12@gmail.com>
2020-04-21 09:01:49 +0200

    homework added

---
+++ week-07/day-01/src/main/java/com/greenfoxacademy/practice_hello_world/PracticeHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.practice_hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeHelloWorldApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PracticeHelloWorldApplication.class, args);
+  }
+
+}
---
+++ week-07/day-01/src/test/java/com/greenfoxacademy/practice_hello_world/PracticeHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.practice_hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeHelloWorldApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
