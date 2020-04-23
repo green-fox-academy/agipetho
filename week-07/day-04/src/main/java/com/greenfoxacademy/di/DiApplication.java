cb05a3a0486414e55e4c0b1fa5f5c81714905fcf
Agnes Petho <apetho12@gmail.com>
2020-04-23 15:22:27 +0200

    hello di added

--- week-07/day-04/src/main/java/com/greenfoxacademy/di/DiApplication.java
+++ week-07/day-04/src/main/java/com/greenfoxacademy/di/DiApplication.java
@@ -1,13 +1,44 @@
 package com.greenfoxacademy.di;
 
+import com.greenfoxacademy.di.helloDI.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiApplication {
+public class DiApplication implements CommandLineRunner {
+
+  private Printer printer;
+
+  @Autowired
+  public DiApplication(Printer printer) {
+    this.printer = printer;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(DiApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("hello");
+  }
 }
+/*
+@SpringBootApplication
+public class DependencyinjectionApplication implements CommandLineRunner {
+  private Printer printer;
+  @Autowired
+  DependencyinjectionApplication(Printer printer) {
+    this.printer = printer;
+  }
+  public static void main(String[] args) {
+    SpringApplication.run(DependencyinjectionApplication.class, args);
+  }
+  @Override
+  public void run(String... args) throws Exception {
+    printer.log("hello");
+  }
+}
+*/
