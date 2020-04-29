b36f41c0c828a9f323f8757ba207a69210c7f9b8
Agnes Petho <apetho12@gmail.com>
2020-04-29 11:06:44 +0200

    reddit project started appproperties set

---
+++ week-08/day-02/extended_ToDo_App/src/main/java/com/greenfoxacademy/extended_to_do_app/ExtendedToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.extended_to_do_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendedToDoAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExtendedToDoAppApplication.class, args);
+  }
+
+}
---
+++ week-08/day-02/extended_ToDo_App/src/test/java/com/greenfoxacademy/extended_to_do_app/ExtendedToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.extended_to_do_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtendedToDoAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
