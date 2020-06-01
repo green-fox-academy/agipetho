35139b2fe11032807cce52917fa10d6cf37f5601
Agnes Petho <apetho12@gmail.com>
2020-06-02 00:01:45 +0200

    hardcoded users included

--- projectPhase-week-00/day-02/src/main/java/com/greenfoxacademy/jwt/JwtApplication.java
+++ projectPhase-week-00/day-02/src/main/java/com/greenfoxacademy/jwt/JwtApplication.java
@@ -1,22 +1,33 @@
 package com.greenfoxacademy.jwt;
 
+import com.greenfoxacademy.jwt.models.User;
+import com.greenfoxacademy.jwt.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwtApplication {
+public class JwtApplication implements CommandLineRunner {
+
+  private UserRepository userRepository;
+
+  @Autowired
+  public JwtApplication(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(JwtApplication.class, args);
   }
 
-//  @Override
-//  public void run(String... args) throws Exception {
-//    if (System.getenv("ACTIVE_PROFILE").equals("Test")){
-//      humanRepository.save(new Human("Bill", "male", 12));
-//      humanRepository.save(new Human("John", "male", 26));
-//      humanRepository.save(new Human("Alexa", "female", 35));
-//    }
-//  }
+  @Override
+  public void run(String... args) throws Exception {
+    if (System.getenv("ACTIVE_PROFILE").equals("test")){
+      userRepository.save(new User("Kati", "kiskati"));
+      userRepository.save(new User("Pali", "nappali"));
+      userRepository.save(new User("Klari", "klarika"));
+    }
+  }
 
 }
